3213acf2fd40c778db255616b741284b92fc2e3a
anuragsingh6 <anuragsingh987@outlook.com>
2023-05-19 22:21:59 +0530

    added minimal search functionality
    search results appear in a dropdown

--- js.js
+++ js.js
@@ -8,26 +8,64 @@ let cimgElement=document.getElementById('cimg');cimgElement.style.background='ri
 cimgElement.style.backgroundSize='contain';cimgElement.style.backgroundImage='url(imgs/icon.svg)';
 
 let darkModeButton=document.getElementById('darkModeButton');let mode='light';
-let darkModeElements1e=document.getElementsByClassName('darkModeElement1e');
+let darkModeElements=document.getElementsByClassName('darkModeElement');
 let cityInput=document.getElementById('cityInput');
 function darkModeToggle(){
     if (mode==='light'){mode='dark';darkModeButton.innerText="☀️";darkModeButton.style.backgroundColor='#e1e1e1';
-        for (let i=0;i<darkModeElements1e.length;i++){darkModeElements1e[i].style.backgroundColor='#1e1e1e';darkModeElements1e[i].style.color='white';}
-        document.getElementById('header').style.backgroundColor='#1f1f1f';
-        cityInput.style.backgroundColor='#1e1e1e';cityInput.style.borderColor='white';cityInput.style.color='white';
+        for (let i=0;i<darkModeElements.length;i++){darkModeElements[i].classList.add('darkModeEnable');}
+        document.getElementById('header').style.backgroundColor='#1f1f1f';cityInput.style.borderColor='white';
+        locationOptions.style.backgroundColor='#1e1e1e';
         document.getElementById('tempUnitChangeButton').style.backgroundColor='#1e1e1e';
     }
     else {mode='light';darkModeButton.innerText="🌙";darkModeButton.style.backgroundColor='#454545';
-        for (let i=0;i<darkModeElements1e.length;i++){darkModeElements1e[i].style.backgroundColor='white';darkModeElements1e[i].style.color='black';}
-        cityInput.style.backgroundColor='white';cityInput.style.borderColor='black';cityInput.style.color='black';
+        for (let i=0;i<darkModeElements.length;i++){darkModeElements[i].classList.remove('darkModeEnable');}
+        document.getElementById('header').style.backgroundColor='white';cityInput.style.borderColor='black';
+        locationOptions.style.backgroundColor='white';
         document.getElementById('tempUnitChangeButton').style.backgroundColor='white';
     }
 }
+let possibleLocations=[];
 function searchLocation(){
-    let query=cityInput.value;let searchURL='https://photon.komoot.io/api/?q='+query+'&layer=city';
-    fetch(searchURL).then((response)=>{console.log(response.json(), response.json().PromiseResult)})
+    let query=cityInput.value;let searchURL='https://photon.komoot.io/api/?q='+query+'&layer=city&limit=10';
+    openLocationOptionsDialog();
+    fetch(searchURL).then((response)=>response.json()).then((result)=>{return result.features;}).then((f)=>{possibleLocations=f;console.log(possibleLocations)
+        locationOptions.innerHTML='';
+        for (let i=0;i<possibleLocations.length;i++){
+            let newLocationOptionItem=document.createElement('div');
+            let newLocationOptionItemText='';
+            if (possibleLocations[i].county===undefined){newLocationOptionItemText=possibleLocations[i].properties.name+' '+possibleLocations[i].properties.osm_value+', '+possibleLocations[i].properties.state+', '+possibleLocations[i].properties.country;}
+            else{newLocationOptionItemText=possibleLocations[i].properties.name+' '+possibleLocations[i].properties.osm_value+', '+possibleLocations[i].properties.county+', '+possibleLocations[i].properties.state+', '+possibleLocations[i].properties.country;}
+            newLocationOptionItem.className='locationOptionItem';newLocationOptionItem.innerText=newLocationOptionItemText;
+            newLocationOptionItem.addEventListener('click', locationItemClicked);locationOptions.appendChild(newLocationOptionItem);
+            }
+        if (possibleLocations.length===0){defaultLocationOptionItem.style.display='block';defaultLocationOptionItem.style.visibility='visible';defaultLocationOptionItem.innerText='Not Found';console.log('Not Found');}
+        else{defaultLocationOptionItem.style.display='none';defaultLocationOptionItem.style.visibility='hidden';defaultLocationOptionItem.innerText='Loading';console.log('Loading...');}
+        })
+    .catch((error)=>console.error('Error: Could not get data:',error))
+    
 }
 cityInput.addEventListener('keyup',searchLocation);
+
+
+function locationItemClicked(){
+    console.log(this.innerText);cityInput.value=this.innerText;
+}
+let locationItemElements=document.getElementsByClassName('locationOptionItem');
+
+let getWeatherButton=document.getElementById('getWeatherButton');
+let locationOptions=document.getElementById('locationOptions');
+let defaultLocationOptionItem=document.getElementById('defaultLocationOptionItem');
+function openLocationOptionsDialog(){
+    if (cityInput.value!==''){locationOptions.style.display='flex';locationOptions.style.visibility='visible';
+    defaultLocationOptionItem.style.display='block';defaultLocationOptionItem.style.visibility='visible';}
+    else{locationOptions.style.display='none';locationOptions.style.visibility='hidden';
+    defaultLocationOptionItem.style.display='none';defaultLocationOptionItem.style.visibility='hidden';}
+}
+
+addEventListener('click', e=>{//console.log(e.target);
+    if (!(e.target===locationOptions)){console.log('adsa');locationOptions.style.display='none';locationOptions.style.visibility='hidden';}
+    else{console.log('aksd');}
+})
 
 
 let city="New York City, USA";let cityElement=document.getElementById('cityName');
